1e43ce344870c7c4100dfdc3df4c1f6089fb174e
Khoa <ohwhynotme1999@gmail.com>
2020-09-26 13:02:26 +0700

    fix attribute xong

--- src/routes/attribute.php
+++ src/routes/attribute.php
@@ -9,17 +9,16 @@ require '../src/services/attributeService.php';
 
 //Get All brand
 $app->get('/api/attributes', function ($request, $response, $args) {
-    // $service = new AttributeService();
-    // echo json_encode($service->GetAllAttributes() );
-    echo json_encode('wtf');
-    // return $response->withHeader('Content-type', 'application/json;charset=UTF-8');
+    $service = new AttributeService();
+    echo json_encode($service->GetAllAttributes() );
+    return $response->withHeader('Content-type', 'application/json;charset=UTF-8');
 });
 
 //Get Single brand
 $app->get('/api/attributes/{id}', function ($request, $response, $args) {
     $id = $request->getAttribute('id');
-    $model = new AttributeModel();
-    echo json_encode($model->GetSingle($id));
+    $service = new AttributeService();
+    echo json_encode($service->GetSingleAttribute($id));
     return $response->withHeader('Content-type', 'application/json;charset=UTF-8');
 });
 //add brand
@@ -27,8 +26,8 @@ $app->post('/api/attributes/add', function ($request, $response, $args) {
     $name           = $request->getParam('name');
     $logo           = $request->getParam('logo');
     $description    = $request->getParam('description');
-    $model = new AttributeModel();
-    echo json_encode($model->Add($name,$logo,$description));
+    $service = new AttributeService();
+    echo json_encode($service->AddAttribute($name,$logo,$description));
     return $response->withHeader('Content-type', 'application/json;charset=UTF-8');
 });
 
@@ -40,8 +39,8 @@ $app->put('/api/attributes/update/{id}', function ($request, $response, $args) {
     $logo   = $request->getParam('logo');
     $description = $request->getParam('description');
 
-    $model = new AttributeModel();
-    echo json_encode($model->Update($id,$name,$logo,$description));
+    $service = new AttributeService();
+    echo json_encode($service->UpdateAttribute($id,$name,$logo,$description));
     return $response->withHeader('Content-type', 'application/json;charset=UTF-8');
 });
 
@@ -50,8 +49,8 @@ $app->delete('/api/attributes/delete/{id}', function ($request, $response, $args
 
     $id = $request->getAttribute('id');
 
-    $model = new AttributeModel();
-    echo json_encode($model->Delete($id));
+    $service = new AttributeService();
+    echo json_encode($service->DeleteAttribute($id));
     return $response->withHeader('Content-type', 'application/json;charset=UTF-8');
 });
 
